e3da5f092edf5f56d9058a17d8b19d06ddac1617
Dominique Septembre <dominique.septembre@gmail.com>
2015-02-16 13:38:55 -0500

    Header added

---
+++ VillageCreate.sql
@@ -0,0 +1,5 @@
+/* **********************************************************
+	DDL Create
+	Schéma MRD:	"Village Vacances VERSION 1"
+	Auteur:		Dominique Septembre - Cégep de Ste-Foy  	
+***********************************************************/
---
+++ VillageSelect.sql
@@ -0,0 +1,5 @@
+/***********************************************************
+	DQL Select
+	Schéma MRD:	"Village Vacances VERSION 1"
+	Auteur:		Dominique Septembre + Charles-Edouard Beaudet - Cégep de Ste-Foy  	
+***********************************************************/
