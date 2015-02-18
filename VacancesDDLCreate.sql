6e691cc89ad06e25a7a26e41f56c8be55156e4e0
Dominique Septembre <dominique.septembre@gmail.com>
2015-02-18 14:23:57 -0500

    add drops to drop remove Drop from create

--- VacancesDDLCreate.sql
+++ VacancesDDLCreate.sql
@@ -3,15 +3,6 @@
 	Schéma MRD:	"Village Vacances VERSION 1"
 	Auteur:		Dominique Septembre + Charles-Edouard Beaudet - Cégep de Ste-Foy  	
 ***********************************************************/
-
-/*
-	Suppression des tables et séquences
-*/
-DROP TABLE LOGEMENT;
-DROP TABLE VILLAGE;
-DROP TABLE CATEGORIE_VILLAGE;
-DROP TABLE TYPE_LOGEMENT;
-
 /*
 	Création des tables et séquences
 */
---
+++ VacancesDDLDrop.sql
@@ -0,0 +1,13 @@
+/* **********************************************************
+	DDL Drop
+	Schéma MRD:	"Village Vacances VERSION 1"
+	Auteur:		Dominique Septembre + Charles-Edouard Beaudet - Cégep de Ste-Foy  	
+***********************************************************/
+
+/*
+	Suppression des tables et séquences
+*/
+DROP TABLE LOGEMENT;
+DROP TABLE VILLAGE;
+DROP TABLE CATEGORIE_VILLAGE;
+DROP TABLE TYPE_LOGEMENT;
