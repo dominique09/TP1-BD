16f5e748bc27d1c4f94b604e5902844e0c5908c9
Charles-Édouard Beaudet <hhuhue@gmail.com>
2015-02-18 14:15:06 -0500

    Plus d'entêtes pour les Selects

--- VillageSelect.sql
+++ VillageSelect.sql
@@ -54,33 +54,67 @@
 /*=======================================================================================*/
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*=============================================================================================================
+	6
+	Produire le calendrier d’occupation du logement 108 du village Casa Dali pour le mois de mars 2015. 
+	Indiquer dans l’ordre : numéro du logement, nom du village, nom du pays, code du type de logement, 
+	description du type de logement, 
+	Version 1 : pour chaque réservation concernant le logement : numéro de la réservation,
+	date de la première nuit (format affichage : jj/mm/aaaa), date de la dernière nuit (format affichage : jj/mm/aaaa).
+	Version 2 : pour chaque date de séjour concernant le logement : numéro de la réservation, 
+	date du séjour (de la nuit occupée) (format affichage : jj/mm/aaaa). Trier par date(s).
+/*=============================================================================================================*/
 
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*=============================================================================================================
+	7
+	 Produire les confirmations pour toutes les réservations effectuées (date de réservation)
+	 entre le 12 et le 20 février 2015 inclusivement. Pour chaque réservation, indiquer dans l’ordre : 
+	 numéro de la réservation, nom du village, date de départ (format affichage : jj/mm/aaaa), 
+	 date de retour (format affichage : jj/mm/aaaa), nombre total de personnes concernées par la réservation, 
+	 numéro du client, nom du client, prénom du client. 
+	 Trier par date de réservation, puis par numéro de réservation. 
+/*=============================================================================================================*/
 
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*============================================================================================================
+	8
+	Produire la liste des clients de Montréal. Pour chaque client habitant Montréal, indiquer dans l’ordre : 
+	numéro du client, nom du client, prénom du client, adresse du client,
+	téléphone domicile du client  (format affichage : (514)412-2296) Trier par no de client. 
+/*===========================================================================================================*/
 
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*=========================================================================================================
+	9
+	Calculer le tarif moyen des nuitées pour chaque catégorie de village. Pour chaque catégorie de village,
+	indiquer dans l’ordre : numéro de la catégorie du village, description de la catégorie du village, 
+	prix moyen par personne et par nuit des logements   (format affichage : 43.64 $Can). 
+	Trier par catégorie de village. 
+/*=========================================================================================================*/
 
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*====================================================================================================
+	10
+	Produire un rapport d’occupation des logements du village Casa-Dali pour le mois de mars 2015.
+	Pour chaque logement du village, indiquer dans l’ordre : numéro du logement, nombre de nuits occupées. 
+	Trier par logement. 
+/*===================================================================================================*/
 
 
 
-/*=======================================================================================
-/*=======================================================================================*/
+/*=========================================================================================================
+	11
+	Produire un rapport d’occupation des logements (nombre de nuits occupées pour une période donnée) 
+	du village Casa-Dali pour la période du 7 mars au 23 mars 2015 inclusivement.  Pour chaque logement 
+	du village dont le taux d’occupation est inférieur à 30%, indiquer dans l’ordre : 
+	taux d’occupation (format affichage : 24%), numéro du logement, code du type de logement, 
+	description du type de logement. Trier par taux d’occupation
+/*=========================================================================================================*/
 
 
 
